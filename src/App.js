8b1b6bbe86ac018e88c7ea25669136ac0e62622e
zehranurkok <zehranurkok@gmail.com>
2024-01-17 00:14:42 +0300

    feat: data fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import { useEffect, useState } from "react";
 
-// Basic usage of useEffect
 function App() {
 
-  const [index, setIndex] = useState(0);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    if (index > 0) {
-      console.log("useEffect!");
-      document.title = ` (${index})`;
+    async function getData() {
+      const res = await fetch("https://jsonplaceholder.org/posts");
+      const data = await res.json();
+      if (data) {setData(data)}
     }
-  }, [index]);
+    getData();
+  }, []);
 
   return (
     <>
-      <h1>{index}</h1>
-      <button onClick={() => setIndex(index + 1)}>Click</button>
+    <ul>
+      {data.map((i) => (
+        <li key={i.id}>{i.title}</li>
+      ))}
+    </ul>
     </>
   );
 }
 
 export default App;
+
+// Basic usage of useEffect
+// function App() {
+
+//   const [index, setIndex] = useState(0);
+//   useEffect(() => {
+//     if (index > 0) {
+//       console.log("useEffect!");
+//       document.title = ` (${index})`;
+//     }
+//   }, [index]);
+
+//   return (
+//     <>
+//       <h1>{index}</h1>
+//       <button onClick={() => setIndex(index + 1)}>Click</button>
+//     </>
+//   );
+// }
+
+// export default App;
