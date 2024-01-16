3467910014e37e13de6c2af8cc55261b5c831586
zehranurkok <zehranurkok@gmail.com>
2024-01-16 23:51:13 +0300

    feat: usage of useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 
-
+// Basic usage of useEffect
 function App() {
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index > 0) {
+      console.log("useEffect!");
+      document.title = ` (${index})`;
+    }
+  }, [index]);
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{index}</h1>
+      <button onClick={() => setIndex(index + 1)}>Click</button>
     </>
   );
 }
