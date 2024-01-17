aa8e33d4844af5f25b14ad33fc14cc5ba511de3b
zehranurkok <zehranurkok@gmail.com>
2024-01-17 15:04:24 +0300

    feat: usage of useReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <>
-      
-    </>
-  )
-}
+// Basic usage of useReducer
+// import { useReducer } from "react";
+// function App() {
+//   const [state, dispatch] = useReducer(reducer, initialState);
 
-export default App;
-// // Fetch Data
+//   return (
+//     <>
+//       <h1>{state.count}</h1>
+//       <button onClick={() => dispatch({type: "increment"})}>+</button>
+//       <button onClick={() => dispatch({type: "decrement"})}>-</button>
+//       <button onClick={() => dispatch({type: "reset"})}>Reset</button>
+//     </>
+//   );
+// }
+// const initialState = { count: 0 };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return {
+//         ...state,
+//         count: state.count + 1,
+//       };
+//     case "decrement":
+//       return {
+//         ...state,
+//         count: state.count - 1,
+//       };
+//     case "reset":
+//       return {
+//         ...state,
+//         count: 0,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default App;
+
+
+// Fetch Data
 // function App() {
 //   const [data, setData] = useState([]);
 
@@ -33,6 +65,7 @@ export default App;
 //   );
 // }
 // export default App;
+
 
 // Basic usage of useEffect
 // function App() {
